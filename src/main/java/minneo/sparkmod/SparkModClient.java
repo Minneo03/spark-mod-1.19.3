33a378a81cc7310837cc2fa81fee82d7df9233cc
Ryan Minneo <ryanminneo03@gmail.com>
2023-07-20 17:25:26 -0400

    Initial commit

---
+++ src/main/java/minneo/sparkmod/SparkModClient.java
@@ -0,0 +1,10 @@
+package minneo.sparkmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SparkModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
